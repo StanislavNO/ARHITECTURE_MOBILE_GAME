423427524b493241be3db99704cabfc55a9c7ee5
stanislavno <stanislav_developer@mail.ru>
2024-08-21 10:59:25 +0500

    +

---
+++ Assets/Source/CodeBase/Services/Input/IInputHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Source.CodeBase
+{
+    public interface IInputHandler
+    {
+        Vector2 Axis { get; }
+        bool IsAttackButtonUp();
+    }
+
+    public class InputHandler : IInputHandler
+    {
+        public Vector2 Axis => throw new System.NotImplementedException();
+
+        public bool IsAttackButtonUp()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
